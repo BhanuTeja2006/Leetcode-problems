29f201b126798654a003b267283208b0c9d98655
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-08-11 10:49:53 +0530

    Time: 6 ms (72.37%) | Memory: 44.6 MB (82.83%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,26 +1,29 @@
-
 class Solution {
-    long resolves(long a, long b, char Operator) {
-        if (Operator == '+') return a + b;
-        else if (Operator == '-') return a - b;
-        else if (Operator == '*') return a * b;
-        return a / b;
-    }
-
     public int evalRPN(String[] tokens) {
-        Stack<Long> stack = new Stack<>();
-        int n = tokens.length;
-        for (int i = 0; i < n; i++) {
-            if (tokens[i].length() == 1 && tokens[i].charAt(0) < 48) {
-                long integer2 = stack.pop();
-                long integer1 = stack.pop();
-                char operator = tokens[i].charAt(0);
-                long resolvedAns = resolves(integer1, integer2, operator);
-                stack.push(resolvedAns);
-            } else {
-                stack.push(Long.parseLong(tokens[i]));
+        Stack <Integer> stack = new Stack<>();
+        List <String> op = Arrays.asList ("+","-","*","/");
+        for(String token:tokens){
+            if(!op.contains(token)){
+                stack.push(Integer.parseInt(token));
+            }
+            else{
+                int b = stack.pop();
+                int a = stack.pop();
+                int c = 0;
+                switch(token){
+                    case "+": c = a+b;
+                                break;
+                    case "-": c = a-b;
+                                break;
+                    case "*": c = a*b;
+                                break;
+                    case "/": c = a/b;
+                                break;
+                }
+                stack.push(c);
             }
         }
-        return stack.pop().intValue();
+        return stack.pop();
+        
     }
 }
