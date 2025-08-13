8c41c63290d686fda5fda074c801d6765984fbbb
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-08-13 11:53:36 +0530

    Time: 4 ms (16.44%) | Memory: 41.7 MB (53.72%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == ']'){
+                String res = "";
+                while(!stack.isEmpty() && !stack.peek().equals("[")){
+                    res = stack.pop() + res;
+                }
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                String repnum = "";
+                while(!stack.isEmpty() && stack.peek().matches("\\d")){
+                    repnum = stack.pop() + repnum;
+                }
+                int n = Integer.parseInt(repnum);
+                String repstr ="";
+                for(int i=0;i<n;i++){
+                    repstr += res;
+                }
+                stack.push(repstr);
+            }
+            else{
+                stack.push(String.valueOf(ch));
+            }  
+        }
+        String decstr = "";
+        for(String r : stack){
+            decstr += r;
+        }
+        return decstr;
+    }
+    
+}
