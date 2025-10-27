f93f38b8e3eb486574905ed64f72e0433e90b323
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-10-27 11:02:19 +0530

    Time: 28 ms (52.99%) | Memory: 45 MB (44.61%) - LeetSync

---
+++ 836-race-car/race-car.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int racecar(int target) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0, 0, 1});
+        Set<String> visited = new HashSet<>();
+        while(!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int moves = curr[0];
+            int pos = curr[1];
+            int speed = curr[2];
+            if(pos == target) return moves;
+            String currState = pos + "," + speed;
+            if(!visited.contains(currState)) {
+                visited.add(currState);
+                queue.offer(new int[] {moves + 1, pos + speed, speed * 2});
+                int newspeed = speed;
+                if(pos + speed < target && speed < 0) newspeed = 1;
+                else if(pos + speed > target && speed > 0) newspeed = -1;
+                queue.offer(new int[] {moves + 1, pos, newspeed});
+        }
+    }
+    return -1;
+}
+}
